d769b263fbfad73683fb62b8eb94963bf1282d05
sameedshaik <sameedshaik13@gmail.com>
2018-09-16 03:03:51 +0530

    ICP4

---
+++ ICP 4/source/Calculator/calculator.js
@@ -0,0 +1,18 @@
+angular.module('CalculatorApp', [])
+    .controller('CalculatorController', function ($scope) {
+        // Write code simple calculator operations
+        $scope.result = function() {
+            if ($scope.operator == '+') {
+                return $scope.a + $scope.b;
+            }
+            if ($scope.operator == '-') {
+                return $scope.a - $scope.b;
+            }
+            if ($scope.operator == '*') {
+                return $scope.a * $scope.b;
+            }
+            if ($scope.operator == '/') {
+                return $scope.a / $scope.b;
+            }
+        };
+    });
